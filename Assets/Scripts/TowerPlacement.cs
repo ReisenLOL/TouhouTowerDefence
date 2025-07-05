d44b5c4351da540a03690c21f5194aef2100f127
ReisenLOL <marisathestealer@gmail.com>
2025-07-04 21:22:04 -0700

    starting stuff

---
+++ Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,16 @@
+using System;
+using UnityEngine;
+
+public class TowerPlacement : MonoBehaviour
+{
+    public Tower selectedTower;
+    public bool isPlacing;
+
+    private void Update()
+    {
+        if (isPlacing)
+        {
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Towers/Tower.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // [stats]
+    public float health;
+    public float maxHealth;
+    public float range;
+    public float damage;
+    public float fireRate;
+}
